cfa2a611b9fc9b16734ac81424024a94aad3840a
Sine Striker <trueful@163.com>
2024-03-27 11:56:21 +0800

    minor tweaks

---
+++ src/widgets/widgettools/cnavframe.cpp
@@ -0,0 +1,234 @@
+#include "cnavframe.h"
+
+#include <QButtonGroup>
+#include <QHBoxLayout>
+#include <QSplitter>
+#include <QStackedLayout>
+
+#include "CTabButton.h"
+
+class CNavFramePrivate : public QObject {
+    Q_DECLARE_PUBLIC(CNavFrame)
+public:
+    CNavFrame *q_ptr;
+
+    QHBoxLayout *layout;
+    QVBoxLayout *leftLayout;
+    QVBoxLayout *buttonLayout;
+
+    QWidget *topWidget;
+    QWidget *bottomWidget;
+
+    QStackedLayout *stack;
+
+    QFrame *leftFrame;
+    QFrame *rightFrame;
+
+    QSplitter *splitter;
+
+    QButtonGroup *btnGroup;
+
+private:
+    void _q_currentChanged(int index);
+    void _q_buttonClicked(int id);
+};
+
+void CNavFramePrivate::_q_currentChanged(int index) {
+    qobject_cast<QAbstractButton *>(buttonLayout->itemAt(index)->widget())->setChecked(true);
+}
+
+void CNavFramePrivate::_q_buttonClicked(int id) {
+    Q_Q(CNavFrame);
+
+    Q_UNUSED(id);
+    q->setCurrentIndex(buttonLayout->indexOf(btnGroup->checkedButton()));
+}
+
+CNavFrame::CNavFrame(QWidget *parent) : QFrame(parent), d_ptr(new CNavFramePrivate()) {
+    Q_D(CNavFrame);
+
+    d->q_ptr = this;
+
+    // Left
+    d->buttonLayout = new QVBoxLayout();
+    d->buttonLayout->setMargin(0);
+    d->buttonLayout->setSpacing(0);
+
+    d->leftLayout = new QVBoxLayout();
+    d->leftLayout->setMargin(0);
+    d->leftLayout->setSpacing(0);
+
+    d->leftLayout->addLayout(d->buttonLayout);
+    d->leftLayout->addStretch();
+
+    d->leftFrame = new QFrame();
+    d->leftFrame->setObjectName("left-frame");
+    d->leftFrame->setLayout(d->leftLayout);
+
+    // Right
+    d->stack = new QStackedLayout();
+    d->stack->setMargin(0);
+
+    d->rightFrame = new QFrame();
+    d->rightFrame->setObjectName("right-frame");
+    d->rightFrame->setLayout(d->stack);
+
+    // Main
+    d->splitter = new QSplitter();
+    d->splitter->setObjectName("splitter");
+    d->splitter->setChildrenCollapsible(false);
+    d->splitter->addWidget(d->leftFrame);
+    d->splitter->addWidget(d->rightFrame);
+
+    d->splitter->setStretchFactor(0, 0);
+    d->splitter->setStretchFactor(1, 1);
+
+    d->layout = new QHBoxLayout();
+    d->layout->setMargin(0);
+    d->layout->setSpacing(0);
+
+    d->layout->addWidget(d->splitter);
+    setLayout(d->layout);
+
+    d->btnGroup = new QButtonGroup(this);
+    d->btnGroup->setExclusive(true);
+
+    d->topWidget = nullptr;
+    d->bottomWidget = nullptr;
+
+    connect(d->stack, &QStackedLayout::currentChanged, d, &CNavFramePrivate::_q_currentChanged);
+    connect(d->btnGroup, &QButtonGroup::idClicked, d, &CNavFramePrivate::_q_buttonClicked);
+}
+
+CNavFrame::~CNavFrame() {
+}
+
+QWidget *CNavFrame::topWidget() const {
+    Q_D(const CNavFrame);
+    return d->topWidget;
+}
+
+void CNavFrame::setTopWidget(QWidget *w) {
+    Q_D(CNavFrame);
+
+    takeTopWidget();
+    d->leftLayout->insertWidget(0, w);
+    d->topWidget = w;
+}
+
+QWidget *CNavFrame::takeTopWidget() {
+    Q_D(CNavFrame);
+
+    QWidget *w = nullptr;
+    if (d->topWidget) {
+        w = d->topWidget;
+        d_ptr->leftLayout->removeWidget(w);
+        d->topWidget = nullptr;
+    }
+    return w;
+}
+
+QWidget *CNavFrame::bottomWidget() const {
+    Q_D(const CNavFrame);
+    return d->bottomWidget;
+}
+
+void CNavFrame::setBottomWidget(QWidget *w) {
+    Q_D(CNavFrame);
+
+    takeBottomWidget();
+    d->leftLayout->addWidget(w);
+    d->bottomWidget = w;
+}
+
+QWidget *CNavFrame::takeBottomWidget() {
+    Q_D(CNavFrame);
+
+    QWidget *w = nullptr;
+    if (d->bottomWidget) {
+        w = d->bottomWidget;
+        d_ptr->leftLayout->removeWidget(w);
+        d->bottomWidget = nullptr;
+    }
+    return w;
+}
+
+QSplitter *CNavFrame::splitter() const {
+    Q_D(const CNavFrame);
+    return d->splitter;
+}
+
+QAbstractButton *CNavFrame::insertWidget(int index, QWidget *w) {
+    Q_D(CNavFrame);
+
+    index = qMin(index, count());
+
+    // New button
+    auto btn = new CTabButton();
+    btn->setCheckable(true);
+    d->btnGroup->addButton(btn);
+
+    // Insert to layouts
+    d->buttonLayout->insertWidget(index, btn);
+    d->stack->insertWidget(index, w);
+
+    return btn;
+}
+
+void CNavFrame::removeWidget(QWidget *w) {
+    Q_D(CNavFrame);
+    removeAt(d->stack->indexOf(w));
+}
+
+void CNavFrame::removeAt(int index) {
+    if (index >= count() || index < 0) {
+        return;
+    }
+    Q_D(CNavFrame);
+
+    // Find button
+    auto btn = qobject_cast<QAbstractButton *>(d->buttonLayout->itemAt(index)->widget());
+    d->btnGroup->removeButton(btn);
+
+    // Remove from layouts
+    d->buttonLayout->removeWidget(btn);
+    d->stack->removeWidget(d->stack->widget(index));
+
+    // Delete button
+    btn->deleteLater();
+}
+
+QWidget *CNavFrame::widget(int index) {
+    Q_D(const CNavFrame);
+    return d->stack->widget(index);
+}
+
+QWidget *CNavFrame::currentWidget() const {
+    Q_D(const CNavFrame);
+    return d->stack->currentWidget();
+}
+
+int CNavFrame::currentIndex() const {
+    Q_D(const CNavFrame);
+    return d->stack->currentIndex();
+}
+
+int CNavFrame::count() const {
+    Q_D(const CNavFrame);
+    return d->stack->count();
+}
+
+int CNavFrame::indexOf(QWidget *w) const {
+    Q_D(const CNavFrame);
+    return d->stack->indexOf(w);
+}
+
+void CNavFrame::setCurrentIndex(int index) {
+    Q_D(CNavFrame);
+    d->stack->setCurrentIndex(index);
+}
+
+void CNavFrame::setCurrentWidget(QWidget *w) {
+    Q_D(const CNavFrame);
+    d->stack->setCurrentWidget(w);
+}
---
+++ src/widgets/widgettools/cnavframe.h
@@ -0,0 +1,57 @@
+#ifndef CNAVFRAME_H
+#define CNAVFRAME_H
+
+#include <QAbstractButton>
+#include <QFrame>
+#include <QSplitter>
+
+#include <QMWidgets/qmwidgetsglobal.h>
+
+class CNavFramePrivate;
+
+class QM_WIDGETS_EXPORT CNavFrame : public QFrame {
+    Q_OBJECT
+    Q_DECLARE_PRIVATE(CNavFrame)
+public:
+    explicit CNavFrame(QWidget *parent = nullptr);
+    ~CNavFrame();
+
+    QWidget *topWidget() const;
+    void setTopWidget(QWidget *w);
+    QWidget *takeTopWidget();
+
+    QWidget *bottomWidget() const;
+    void setBottomWidget(QWidget *w);
+    QWidget *takeBottomWidget();
+
+    QSplitter *splitter() const;
+
+    /**
+     * @brief Button which will control the widget visibility, set text or icon after call
+     *
+     */
+    inline QAbstractButton *addWidget(QWidget *w);
+    QAbstractButton *insertWidget(int index, QWidget *w);
+    void removeWidget(QWidget *w);
+    void removeAt(int index);
+
+    QWidget *widget(int index);
+    QWidget *currentWidget() const;
+    int currentIndex() const;
+    int count() const;
+    int indexOf(QWidget *w) const;
+
+    void setCurrentIndex(int index);
+    void setCurrentWidget(QWidget *w);
+
+protected:
+    QScopedPointer<CNavFramePrivate> d_ptr;
+
+    friend class CNavFramePrivate;
+};
+
+inline QAbstractButton *CNavFrame::addWidget(QWidget *w) {
+    return insertWidget(count(), w);
+}
+
+#endif // CNAVFRAME_H
